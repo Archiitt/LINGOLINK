3fd86c3826c5487dc1e4f9563acf4bbec0f8e5be
ARCHIT <145273087+Archiitt@users.noreply.github.com>
2023-09-25 00:06:11 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,18 @@
+// Selecting the DOM element with the class "from-text"
+const fromText = document.querySelector(".from-text"),
+
+// Selecting the DOM element with the class "to-text"
+toText = document.querySelector(".to-text"),
+
+// Selecting the DOM element with the class "exchange" (presumably an icon for exchange)
+exchangeIcon = document.querySelector(".exchange"),
+
+// Selecting all DOM elements of type "select"
+selectTags = document.querySelectorAll("select"),
+
+// Selecting all DOM elements with the class "row" and the element "i" (presumably icons)
+icons = document.querySelectorAll(".row i");
+
+// Selecting the DOM button element
+translateBtn = document.querySelector("button");
 
